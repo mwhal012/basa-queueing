d693493279550b1406b674b3210023e6493114ac
mwhal012 <145363243+mwhal012@users.noreply.github.com>
2023-11-15 11:15:18 -0500

    table adjustments, c_R as a function

--- Month-PeriodOfWeek-TimeOfDayCluster.R
+++ Month-PeriodOfWeek-TimeOfDayCluster.R
@@ -1,7 +1,11 @@
-library(dplyr)
+library(conflicted)
+library(tidyverse)
+library(rlang)
+conflicts_prefer(dplyr::filter)
 
-passenger_data <- read.csv("R Files/Data/passenger_data.csv")
-data = passenger_data
+unzip(zipfile = "passenger_data.zip")
+data = "passenger_data.csv" |>
+  read_csv()
 
 table18 <- data %>%
   group_by(Month, Period_of_Week, Time_of_Day) %>%
@@ -22,11 +26,12 @@ table20 <- data %>%
     C0_Distribution = prop.table(table(C0)) * 100,
     C0_Value = as.numeric(names(table(C0)))
   ) %>%
-  mutate(C0_Distribution = sprintf("%.2f%%", C0_Distribution))
+  mutate(C0_Distribution = sprintf("%.2f%%", C0_Distribution)) # needs a reframe
 
 table23 <- data %>%
   group_by(Month, Period_of_Week, Time_of_Day) %>%
   summarise(
+    Count = n(),
     Wait_Avg = mean(Wait_Time, na.rm = TRUE),
     LessThan5 = mean(Wait_Time < 5, na.rm = TRUE) * 100,
     LessThan10 = mean(Wait_Time < 10, na.rm = TRUE) * 100,
@@ -34,36 +39,95 @@ table23 <- data %>%
     LessThan20 = mean(Wait_Time < 20, na.rm = TRUE) * 100,
     LessThan25 = mean(Wait_Time < 25, na.rm = TRUE) * 100,
     LessThan30 = mean(Wait_Time < 30, na.rm = TRUE) * 100
-  )
+  ) |>
+  filter(!is.na(Wait_Avg))
 
 table25 <- data %>%
   group_by(Month, Period_of_Week, Time_of_Day) %>%
-  summarise(Arrival_Rate = n() / (360 * n_distinct(Departure_Date)),
-            Wait_Avg = mean(Wait_Time, na.rm = TRUE),
-            Est_Serv = ((Wait_Avg * Arrival_Rate) + sqrt((Wait_Avg * Arrival_Rate)^2 + 4 * (Wait_Avg * Arrival_Rate))) / (2 * Wait_Avg),
-            Est_Phi = Wait_Avg * (Est_Serv - Arrival_Rate),
-            EstLessThan5 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*5))*100,
-            EstLessThan10 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*10))*100,
-            EstLessThan15 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*15))*100,
-            EstLessThan20 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*20))*100,
-            EstLessThan25 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*25))*100,
-            EstLessThan30 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*30))*100
-  )
+  summarise(
+    Arrival_Rate = n() / (360 * n_distinct(Departure_Date)),
+    Wait_Avg = mean(Wait_Time, na.rm = TRUE),
+    term = Wait_Avg * Arrival_Rate,
+    Est_Serv = (term + sqrt(term^2 + 4 * term)) / (2 * Wait_Avg),
+    Est_Rho = Wait_Avg * (Est_Serv - Arrival_Rate),
+    EstLessThan5 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*5))*100,
+    EstLessThan10 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*10))*100,
+    EstLessThan15 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*15))*100,
+    EstLessThan20 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*20))*100,
+    EstLessThan25 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*25))*100,
+    EstLessThan30 = (1 - (Arrival_Rate/Est_Serv)*exp(-(Est_Serv-Arrival_Rate)*30))*100
+  ) |>
+  select(!c(Arrival_Rate:term)) |>
+  filter(!is.na(Est_Serv))
 
-table29 <- data %>%
-  group_by(Month, Period_of_Week, Time_of_Day) %>%
-  summarise(Arrival_Rate = n() / (360 * n_distinct(Departure_Date)),
-            Wait_Avg = mean(Wait_Time, na.rm = TRUE),
-            Est_Serv = ((Wait_Avg * Arrival_Rate) + sqrt((Wait_Avg * Arrival_Rate)^2 + 4 * (Wait_Avg * Arrival_Rate))) / (2 * Wait_Avg),
-            Avg_C0 = mean(C0),
-            Arr_Rate_per_server = (Arrival_Rate/Avg_C0),
-            Est_Serv_per_server = (Est_Serv/Avg_C0)
-  )
+tab_29 = data |>
+  group_by(Month, Period_of_Week, Time_of_Day) |>
+  summarise(
+    Avg_C0 = mean(C0),
+    Arrival_Rate = n() / (360 * n_distinct(Departure_Date)),
+    Wait_Avg = mean(Wait_Time, na.rm = TRUE),
+    term = Wait_Avg * Arrival_Rate,
+    Est_Serv = (term + sqrt(term^2 + 4 * term)) / (2 * Wait_Avg),
+    Arr_Rate_per_server = Arrival_Rate / Avg_C0,
+    Est_Serv_per_server = Est_Serv / Avg_C0
+  ) |>
+  select(!c(term, Wait_Avg)) |>
+  filter(!is.na(Est_Serv))
 
-table29 <- table29[-c(22), ]
+plot(tab_29$Arr_Rate_per_server, tab_29$Est_Serv_per_server)
 
-mean((table29$Arr_Rate_per_server - table29$Est_Serv_per_server)^2)
+mod = tab_29 |>
+  lm(formula = Est_Serv ~ 0 + Avg_C0 + Arrival_Rate)
+summary(mod)
 
-model = lm(formula = Est_Serv_per_server ~ Arr_Rate_per_server, data = table29)
-plot(table29$Arr_Rate_per_server,table29$Est_Serv_per_server)
-summary(model)
+coef = coef(mod)
+
+est_perf = function(c, lambda, mins = 5) {
+  mu_R = as.numeric(t(coef) %*% c(c, lambda))
+  return(1 - (lambda/mu_R)*exp((lambda - mu_R)*mins))
+}
+
+tab_32 = data |>
+  group_by(Month, Period_of_Week, Time_of_Day) |>
+  summarise(
+    Arrival_Rate = n() / (360 * n_distinct(Departure_Date)),
+    Wait_Avg = mean(Wait_Time, na.rm = TRUE),
+    term = Wait_Avg *Arrival_Rate,
+    Est_Serv = (term + sqrt(term^2 + 4 * term)) / (2 * Wait_Avg),
+    Est_Rho = Wait_Avg * (Est_Serv - Arrival_Rate),
+    perf_lt5 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 5) * 100,
+    perf_lt10 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 10) * 100,
+    perf_lt15 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 15) * 100,
+    perf_lt20 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 20) * 100,
+    perf_lt25 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 25) * 100,
+    perf_lt30 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 30) * 100
+  ) |>
+  select(!c(Arrival_Rate:term)) |>
+  filter(!is.na(Est_Serv))
+
+pred_serv = function(p = 1, lambda, mins = 5) {
+  term = lambda * mins
+  arg = (term/(1 - p))*exp(term)
+  return((lamW::lambertW0(arg) - coef[2]*term)/(coef[1]*mins))
+}
+
+tab_34 = data |>
+  group_by(Month, Period_of_Week, Time_of_Day) |>
+  summarise(
+    Count = n(),
+    Avg_C0 = mean(C0),
+    Arrival_Rate = n() / (360 * n_distinct(Departure_Date)),
+    Wait_Avg = mean(Wait_Time, na.rm = TRUE),
+    term = Wait_Avg *Arrival_Rate,
+    Est_Serv = (term + sqrt(term^2 + 4 * term)) / (2 * Wait_Avg),
+    perf_lt5 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 5) * 100,
+    perf_lt10 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 10) * 100,
+    perf_lt15 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 15) * 100,
+    perf_lt20 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 20) * 100,
+    perf_lt25 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 25) * 100,
+    perf_lt30 = est_perf(c = Est_Serv, lambda = Arrival_Rate, mins = 30) * 100,
+    c_pred_lt5 = pred_serv(p = perf_lt5/100, lambda = Arrival_Rate, mins = 5),
+    c_pred_lt10 = pred_serv(p = perf_lt10/100, lambda = Arrival_Rate, mins = 10)
+  ) |>
+  select(!term) |>
+  filter(!is.na(Wait_Avg))
